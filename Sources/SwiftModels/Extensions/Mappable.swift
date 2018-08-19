ecf1e01969afdf86af7684b723297bd76aaf4d9c
Vito Librarius <vitolibrarius@gmail.com>
2018-08-19 12:51:25 -0600

    Made the encoding extensions public

--- Sources/SwiftModels/Extensions/Mappable.swift
+++ Sources/SwiftModels/Extensions/Mappable.swift
@@ -23,13 +23,13 @@ extension Mappable {
 }
 
 extension Encodable {
-    func encoded() throws -> Data {
+    public func encoded() throws -> Data {
         return try JSONEncoder().encode(self)
     }
 }
 
 extension Data {
-    func decoded<T: Decodable>() throws -> T {
+    public func decoded<T: Decodable>() throws -> T {
         return try JSONDecoder().decode(T.self, from: self)
     }
 }
